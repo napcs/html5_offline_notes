c3915aa440ff3e58610d5299b1dc467d70b120d9
Brian Hogan <brianhogan@napcs.com>
2010-10-05 22:53:43 -0500

    db creation

--- javascripts/notes.js
+++ javascripts/notes.js
@@ -5,6 +5,7 @@ $(function(){
   $("#delete").hide();
   
   
+  
   $("#new").click(function(e){
     $("#form").show();
     $("#notes").hide();
@@ -16,6 +17,38 @@ $(function(){
     $("#notes").show();
     
   });
+  
+  
+  var db = null;
+
+  // Creates a connection to the local database
+  connectToDB = function()
+  {
+     db = window.openDatabase('tccc_notes', '1.0',
+                                     'TCCC Notes Database', 1024*1024*3);
+  };
+
+
+
+  //Create the table method
+  createNotesTable = function()
+  {
+    db.transaction(function(tx){
+      tx.executeSql(
+        "CREATE TABLE notes (id INTEGER \
+         PRIMARY KEY, title TEXT, note TEXT)", [],
+        function(){ alert('Notes database created successfully!'); },
+        function(tx, error){ alert(error.message); } );
+    });
+  };
+  
+  connectToDB();
+  createNotesTable();
+  
+  
+  
+  
+  
   
   
 });
